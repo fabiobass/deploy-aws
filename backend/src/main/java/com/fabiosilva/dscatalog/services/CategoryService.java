2d4bb310b5881dab358711d0263ce35c6521b128
Fabio Silva <fsilva.developer@gmail.com>
2022-05-18 20:39:10 -0300

    DTO

--- backend/src/main/java/com/fabiosilva/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fabiosilva/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.fabiosilva.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fabiosilva.dscatalog.dto.CategoryDTO;
 import com.fabiosilva.dscatalog.entities.Category;
 import com.fabiosilva.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true) // readOnly so para leitura
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+				
 	}
 }
