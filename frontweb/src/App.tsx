f3b022b17dd601477d2685913c4d8fb2cce945c8
Fabio Silva <fsilva.developer@gmail.com>
2022-07-08 23:24:19 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Nega</h1>
+    <>
+    <Navbar />
+    <h1>Em breve um site Erica Festas</h1>
+    </>
   );
 }
 
