4588ab52fd8221b0cb1ac17e5ee4084ac8687834
Fabio Silva <fsilva.developer@gmail.com>
2022-07-10 16:58:54 -0300

    Routes

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const ProductPrice = () =>{
+
+    return (
+        <div className='product-prive-container'>
+            <span>R$</span>
+            <h3>2345,00</h3>
+        </div>
+    );
+}
+
+export default ProductPrice;
---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,29 @@
+import ProducCard from "components/ProductCard";
+
+const Catalog = () => {
+
+    return (
+            <div className="container my-4">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 ">
+                        <ProducCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 ">
+                        <ProducCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 ">
+                        <ProducCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 ">
+                        <ProducCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 ">
+                        <ProducCard />
+                    </div>
+                </div>
+                
+            </div>
+    );
+}
+
+export default Catalog;
