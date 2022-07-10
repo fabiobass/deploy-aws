edcb13c694b4ae7c3efc88ab139530d2b1709722
Fabio Silva <fsilva.developer@gmail.com>
2022-07-09 23:35:26 -0300

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,10 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <h1>Em breve um site Erica Festas</h1>
-    </>
+    <Home />
   );
 }
 
